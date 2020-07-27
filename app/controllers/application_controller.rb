a140e02cc442388dd9e2d9880b0dcaaed171d6d6
mike <mike@mikes-MacBook-Pro.local>
2020-07-27 00:46:55 -0400

    Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   def status
+    YoutubeDlRequest.create
     render json: { hello: "world", deploy: "test", pending_request_count: YoutubeDlRequest.count }, status: 200
   end
 end
