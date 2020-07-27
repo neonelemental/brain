0f05e415caa39533df971ca8addc9b353fa8c1e5
mike <mike@mikes-MacBook-Pro.local>
2020-07-27 00:03:09 -0400

    Fixes for deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo docker-compose down
-sudo docker-compose run web bundle && rails db:migrate
-sudo docker-compose up
+sudo docker-compose run web bundle
+sudo docker-compose run web rails db:migrate
+sudo docker-compose up -d
