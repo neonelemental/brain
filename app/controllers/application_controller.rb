d6ce8e986f79a3f5481b7d4556d7a229ef6b31c6
mike <mike@mikes-MacBook-Pro.local>
2020-07-26 23:19:18 -0400

    Deploy test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def status
-    render json: { hello: "world" }, status: 200
+    render json: { hello: "world", deploy: "test" }, status: 200
   end
 end
