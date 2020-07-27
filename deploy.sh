f04fe651669b65043a24c945d83c09c1844315e0
mike <mike@mikes-MacBook-Pro.local>
2020-07-27 00:53:20 -0400

    testing new deploy measures

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker-compose down
-docker-compose run web bundle
-docker-compose run web rails db:migrate
-docker-compose up -d
+docker-compose build web
+docker-compose up --no-deps -d web
