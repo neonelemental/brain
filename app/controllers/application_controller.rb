8aa75fe212b4d55077a30117467948c490deef03
mike <mike@mikes-MacBook-Pro.local>
2020-07-27 00:53:45 -0400

    Dont create requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   def status
-    YoutubeDlRequest.create
     render json: { hello: "world", deploy: "test", pending_request_count: YoutubeDlRequest.count }, status: 200
   end
 end
